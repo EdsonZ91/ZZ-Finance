2ede776ba25a0e986b9a2f90932f7c43a3fba436
EdsonZ91 <edsonfzeferino@hotmail.com>
2024-05-20 22:54:48 +0000

    começando novo project add cabeçario

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  
+
+  return (
+   <div className='cabeçario'> 
+   <h1> ZZ-Finance</h1>
+   <button className='firstb'> Nova transação</button>
+
+   </div>
+
+  
+   
+
+   
+  )
+}
+
+export default App
