7dc31bdd6bb12f54a88074d78883cf6763efe2d2
EdsonZ91 <edsonfzeferino@hotmail.com>
2024-05-21 23:06:33 +0000

    majorBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-
+import"./App.css"
 function App() {
   
 
   return (
-   <div className='cabeçario'> 
-   <h1> ZZ-Finance</h1>
-   <button className='firstb'> Nova transação</button>
+    <div className="majorBox"> 
+      <div className="Box">
+        <h1> ZZ-Finance</h1>
+        <button className="firstBu"> Nova transação</button>
 
-   </div>
+      </div>
+    </div>
 
   
    
