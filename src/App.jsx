0946761e4de78517c30a9dd46a072e54ede2194f
EdsonZ91 <edsonfzeferino@hotmail.com>
2024-05-22 19:58:21 +0000

    new Save

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ function App() {
         <div className="majorBox">
             <div className="Box">
                 <h1>ZZ-Finance</h1>
+
+                <div className="space"></div>
+
+
+
                  <button className="firstBu">Nova transação</button>
              </div>
              <div className="cardsBank">
                 <form>
-                    <input></input>
-                    <input></input>
-                    <input></input>
+                    <input />
+
+                    <input />
+
+                    <input />
 
                 </form>
              </div>
