2f55cdfd4f5c86200d8c62289e1c4edd021ee0a1
EdsonZ91 <edsonfzeferino@hotmail.com>
2024-05-22 18:11:08 +0000

    fresh and clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import"./App.css"
 function App() {
-  
+    return(
+        <div className="majorBox">
+            <div className="Box">
+                <h1>ZZ-Finance</h1>
+                 <button className="firstBu">Nova transação</button>
+             </div>
+             <div className="cardsBank">
+                <form>
+                    <input></input>
+                    <input></input>
+                    <input></input>
 
-  return (
-    <div className="majorBox"> 
-      <div className="Box">
-        <h1> ZZ-Finance</h1>
-        <button className="firstBu"> Nova transação</button>
-
-      </div>
-    </div>
-
-  
-   
-
-   
-  )
+                </form>
+             </div>
+        </div>
+    )
 }
 
 export default App
